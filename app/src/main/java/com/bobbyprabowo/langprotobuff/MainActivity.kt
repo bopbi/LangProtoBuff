9c0f454e117af2c66bb53d6a68012639cc9352a2
Bobby Prabowo <prabowo.bobby@gmail.com>
2025-02-11 21:34:50 +0900

    add datastore

--- app/src/main/java/com/bobbyprabowo/langprotobuff/MainActivity.kt
+++ app/src/main/java/com/bobbyprabowo/langprotobuff/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.bobbyprabowo.langprotobuff
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
 import com.example.protobuf.TranslationList
+import com.google.protobuf.InvalidProtocolBufferException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private val dataStore: DataStore<TranslationList> by dataStore(
+        fileName = "translation.pb",
+        serializer = TranslationListSerializer(),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,18 +38,34 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val protoMessage = readProtobufBinaryFromAssets(this, "schema.bin")
-        Toast.makeText(this, protoMessage?.getTranslations(1)?.jp, Toast.LENGTH_LONG).show()
+        CoroutineScope(Dispatchers.IO + SupervisorJob()).launch {
+            this@MainActivity.assets.open("schema.bin").use { inputStream ->
+                dataStore.updateData { translationList ->
+                    translationList.toBuilder().mergeFrom(inputStream).build()
+                }
+            }
+            dataStore.data.first().let { translationList ->
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, translationList.getTranslations(0)?.en, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
-    private fun readProtobufBinaryFromAssets(context: Context, fileName: String): TranslationList? {
-        return try {
-            context.assets.open(fileName).use { inputStream ->
-                TranslationList.parseFrom(inputStream)
+    class TranslationListSerializer : Serializer<TranslationList> {
+
+        override val defaultValue: TranslationList = TranslationList.getDefaultInstance()
+
+        override suspend fun readFrom(input: InputStream): TranslationList {
+            return try {
+                TranslationList.parseFrom(input)
+            } catch (exception: InvalidProtocolBufferException) {
+                throw CorruptionException("Cannot read proto.", exception)
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
+        }
+
+        override suspend fun writeTo(t: TranslationList, output: OutputStream) {
+            t.writeTo(output)
         }
     }
 }
